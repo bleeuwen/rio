4a171273630362eabe2f6e642d2d09fc0e06b570
bleeuwen <bart@leeuwennet.nl>
2013-10-22 12:08:30 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rio::Application.config.secret_key_base = 'a3345c3a36dd0316a88f6e6a850b0fbb315ed9691ba6a160a29c8538fbc978d7180c4a7cccff3d3e926cdfcad980fe1acf5babbc0a921ce5f0868efe28e6df03'
